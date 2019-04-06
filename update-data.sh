b001ed6c87a907bf42a27f009a5484e804312b9d
roostaq <48365367+roostaq@users.noreply.github.com>
2019-04-06 11:00:18 +0000

    Update update-data.sh

--- update-data.sh
+++ update-data.sh
@@ -3,16 +3,9 @@
 set -e
 pwd
 
-remote="https://$GH_TOKEN@github.com/roostaq/irasutoya-data.git"
-
-git config --global user.email "$GH_EMAIL" > /dev/null 2>&1
-git config --global user.name  "$GH_NAME" > /dev/null 2>&1
-
-git remote add origin "$remote"
+touch test
 
 git add .
 git commit -m "Monthly data update $(date +"%T")"
 
-git push --force --quiet origin master > /dev/null 2>&1
-
-echo "Finished!"
+git push --force --quiet origin master 
